362f525e1a899b85af5ac8690f5cdc36418b5c4a
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-23 02:38:56 +0800

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,8 +1,8 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
+$apiLink = ""; // https://gogoanimeapi-chi.vercel.app
 if (date("d") > 15){
-    $apiLink = ""; // same goes for here
+    $apiLink = ""; // https://gogoanimeapi-chi.vercel.app
 }
 ?>
