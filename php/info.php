62215be53988947ed20397e13462804564b8dc75
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-24 19:11:58 +0800

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,8 +1,8 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "AnimXer";
-$apiLink = ""; // https://gogoanimeapi-chi.vercel.app
+$apiLink = "https://gogoanimeapi-chi.vercel.app";
 if (date("d") > 15){
-    $apiLink = ""; // https://gogoanimeapi-chi.vercel.app
+    $apiLink = "https://gogoanimeapi-chi.vercel.app";
 }
 ?>
