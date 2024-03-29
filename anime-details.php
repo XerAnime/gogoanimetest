f4af2e1f41ec52aa230e98da0834b8392bd455f5
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-03-29 18:41:05 +0800

    Update anime-details.php

--- anime-details.php
+++ anime-details.php
@@ -120,7 +120,7 @@ $episodeArray = $fetchDetails['episode_id'];
                   (function () {  // DON'T EDIT BELOW THIS LINE
                     var d = document, s = d.createElement('script');
 
-                    s.src = '//animxer-5.disqus.com/embed.js';
+                    s.src = 'https://animxer-5.disqus.com/embed.js';
 
                     s.setAttribute('data-timestamp', +new Date());
                     (d.head || d.body).appendChild(s);
