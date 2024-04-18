51631fe5f1af317f69c14640f53b2fde4178e914
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-04-18 19:06:25 +0800

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,8 +1,8 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "AnimXer";
-$apiLink = "https://animeapi-zcuf.onrender.com";
+$apiLink = "https://animeapi-sqk3.onrender.com";
 if (date("d") > 15){
-    $apiLink = "https://animeapi-zcuf.onrender.com";
+    $apiLink = "https://animeapi-sqk3.onrender.com";
 }
 ?>
